39e0256ea207a6ecffa775302a657a69eb6c5773
nv21065-abdulaziz-soroor <nv21065@nvtc.edu.bh>
2022-12-26 10:17:19 +0400

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -251,7 +251,7 @@ export default function Home({ articles }) {
     <>
       <Head>
         <title>
-          Spencer Sharp - Software designer, founder, and amateur astronaut
+          Abdulaziz - NV21065
         </title>
         <meta
           name="description"
